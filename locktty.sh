9cd4309b4961f92143a99763998344ad093afc02
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-04-18 00:35:44 -0700

    add timer

--- locktty.sh
+++ locktty.sh
@@ -2,8 +2,21 @@
 
 #trap ‘‘ 1  2  3  18
 trap "" SIGHUP  SIGINT  SIGQUIT SIGCONT
+exitLocktty() {
+    stty echo
+    exit 0
+}
+trap 'exitLocktty' USR1
 
+checkTime () {
+    set -e
+    sleep 5m
+    kill -USR1 $$
+    exit 0 
+}
 stty   -echo
+
+
 echo   “Enter key:  ”
 read key_1
 echo
@@ -11,14 +24,16 @@ echo   “Again:  ”
 read key_2
 echo
 
+
+
 if [ "$key_1" == "$key_2" ]
 then
+      checkTime &
       tput clear
       while [ "$key_3" != "$key_2" ]
       do
           echo "enter password:  "
-          sleep 1
-
+          sleep 1 
           read key_3
       done
       echo "tty is now unlocked   "
